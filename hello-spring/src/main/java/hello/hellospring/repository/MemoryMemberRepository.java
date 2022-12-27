bad3c47a31084d36f3fb2d840f17308dc4749adb
sok5188 <sok5188@gmail.com>
2022-12-27 16:02:53 +0900

    spring study day2

--- hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -12,6 +12,7 @@ public class MemoryMemberRepository implements MemberRepository{
     public Member save(Member member) {
         member.setId(++sequence);
         store.put(member.getId(), member);
+
         return member;
     }
 

d19961cf6b5c8c91f0ba914f4ca425e3cbe12e84
sok5188 <sok5188@gmail.com>
2023-01-11 23:00:45 +0900

    GameService 테스트 케이스 작성 / 웹페이지 css 적용

--- guess_music/src/test/java/com/example/guess_music/service/GameServiceTest.java
+++ guess_music/src/test/java/com/example/guess_music/service/GameServiceTest.java
@@ -1,38 +1,56 @@
 package com.example.guess_music.service;
 
+import com.example.guess_music.domain.Answers;
+import com.example.guess_music.domain.Game;
+import com.example.guess_music.domain.Result;
 import com.example.guess_music.repository.AnswerRepository;
 import com.example.guess_music.repository.GameRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
 @Transactional
 class GameServiceTest {
-    public GameServiceTest(AnswerRepository answerRepository, GameRepository gameRepository) {
-        this.answerRepository = answerRepository;
-        this.gameRepository = gameRepository;
-    }
-    @Autowired
-    private final AnswerRepository answerRepository;
-    @Autowired
-    private final GameRepository gameRepository;
     @Autowired
     GameService gameService;
+
+    @Autowired
+    ManagerService managerService;
     @Test
     void 정답확인() {
+        Game game=new Game();
+        game.setTitle("forTestGame");
+        Long gameIndex = managerService.join(game);
+        List<String> ans=new ArrayList<>();
+        ans.add("ForTest2");
+        int i = managerService.storeFile(ans, "ForTest2", "ForTest2", gameIndex);
 
+        Result result = gameService.getResult("ForTest2", gameIndex, i);
+        assertThat(result.getResult()).isEqualTo("Right");
     }
 
     @Test
-    void getGameSize() {
+    void 힌트확인() {
+        Game game=new Game();
+        game.setTitle("forTestGame");
+        Long gameIndex = managerService.join(game);
+
+        List<String> ans=new ArrayList<>();
+        ans.add("테스트게임");
+        int seq=managerService.storeFile(ans,"sirong","ㅌㅅㅌㄱㅇ",gameIndex);
+
+        assertThat(gameService.getHint("singer",gameIndex,seq)).isEqualTo("sirong");
+        assertThat(gameService.getHint("initial",gameIndex,seq)).isEqualTo("ㅌㅅㅌㄱㅇ");
+
     }
 
-    @Test
-    void getHint() {
-    }
-
-    @Test
-    void getGameList() {
-    }
 }

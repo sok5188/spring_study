4cfff34087d1461ea4b831bbb834daaf4ffd1e6b
sok5188 <sok5188@gmail.com>
2023-02-07 22:45:11 +0900

    정답 판별 및 스킵 확인을 위해 ENUM 타입 추가

--- guess_music/src/main/java/com/example/guess_music/domain/ChatMessage.java
+++ guess_music/src/main/java/com/example/guess_music/domain/ChatMessage.java
@@ -1,7 +1,7 @@
 package com.example.guess_music.domain;
 public class ChatMessage {
     public enum MessageType {
-        ENTER, TALK,START,LEAVE,ANSWER
+        ENTER, TALK,START,LEAVE,ANSWER,SKIP
     }
 
     private MessageType type;

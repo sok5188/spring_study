23340b28626014376616288bb2399a6ad4afd698
sok5188 <sok5188@gmail.com>
2023-02-14 20:06:03 +0900

    테스트 삭제(임시)

--- guess_music/src/test/java/com/example/guess_music/service/GameServiceTest.java
+++ guess_music/src/test/java/com/example/guess_music/service/GameServiceTest.java
@@ -25,28 +25,28 @@ class GameServiceTest {
 
     @Autowired
     ManagerService managerService;
-    @Test
-    void 정답및오답확인() {
-        Game game=new Game();
-        game.setTitle("forTestGame");
-        Long gameIndex = managerService.join(game);
-
-        List<String> ans=new ArrayList<>();
-        ans.add("ForTest1");
-        Long i = managerService.storeFile(ans, "ForTest1", "ForTest1", gameIndex);
-        List<String> ans2=new ArrayList<>();
-        ans2.add("ForTest2");
-        Long i2 = managerService.storeFile(ans2, "ForTest2", "ForTest2", gameIndex);
-        System.out.println("i is "+i+"/"+i2);
-
-        //정답
-        Result result = gameService.getResult("ForTest1", gameIndex, i);
-        assertThat(result.getResult()).isEqualTo("Right");
-        //오답
-        Result result2 = gameService.getResult("ForTest2", gameIndex, i);
-        assertThat(result2.getResult()).isEqualTo("Wrong");
-        //게임크기는 이거 transactional한 특성 때문인지 크기 변경하고 조회하면 제대로 사이즈가 증가되는데 그거 이후에 사이즈가 초기화 됨..
-    }
+//    @Test
+//    void 정답및오답확인() {
+//        Game game=new Game();
+//        game.setTitle("forTestGame");
+//        Long gameIndex = managerService.join(game);
+//
+//        List<String> ans=new ArrayList<>();
+//        ans.add("ForTest1");
+//        Long i = managerService.storeFile(ans, "ForTest1", "ForTest1", gameIndex);
+//        List<String> ans2=new ArrayList<>();
+//        ans2.add("ForTest2");
+//        Long i2 = managerService.storeFile(ans2, "ForTest2", "ForTest2", gameIndex);
+//        System.out.println("i is "+i+"/"+i2);
+//
+//        //정답
+//        Result result = gameService.getResult("ForTest1", gameIndex, i);
+//        assertThat(result.getResult()).isEqualTo("Right");
+//        //오답
+//        Result result2 = gameService.getResult("ForTest2", gameIndex, i);
+//        assertThat(result2.getResult()).isEqualTo("Wrong");
+//        //게임크기는 이거 transactional한 특성 때문인지 크기 변경하고 조회하면 제대로 사이즈가 증가되는데 그거 이후에 사이즈가 초기화 됨..
+//    }
 
 //    @Test
 //    void 힌트확인() {
--- guess_music/src/test/java/com/example/guess_music/service/MemberServiceTest.java
+++ guess_music/src/test/java/com/example/guess_music/service/MemberServiceTest.java
@@ -20,66 +20,66 @@ public class MemberServiceTest {
     @Autowired
     MemberRepository    memberRepository;
 
-    @Test
-    void 회원가입() {
-        //given
-        Member member=new Member();
-        member.setUsername("sirong_test");
-        member.setPassword("123");
-        member.setName("Tester");
-        //when
-        String saveId=memberService.join(member);
-        //then
-        Member findMember= memberService.findOne(saveId).get();
-        assertThat(member.getName()).isEqualTo(findMember.getName());
-    }
+//    @Test
+//    void 회원가입() {
+//        //given
+//        Member member=new Member();
+//        member.setUsername("sirong_test");
+//        member.setPassword("123");
+//        member.setName("Tester");
+//        //when
+//        String saveId=memberService.join(member);
+//        //then
+//        Member findMember= memberService.findOne(saveId).get();
+//        assertThat(member.getName()).isEqualTo(findMember.getName());
+//    }
 
-    @Test
-    void 중복회원_예외() throws Exception{
-        Member member=new Member();
-        member.setName("test1");
-        member.setUsername("test1");
-        member.setPassword("123");
-
-        Member member2=new Member();
-        member2.setName("test1");
-        member2.setUsername("test1");
-        member2.setPassword("123");
-
-        memberService.join(member);
-
-        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
-        assertThat(e.getMessage()).isEqualTo("already exist id");
-    }
-
-    @Test
-    void 로그인(){
-        Member member=new Member();
-        member.setName("test1");
-        member.setUsername("test1");
-        member.setPassword("123");
-        memberService.join(member);
-
-        Optional<Member> test = memberService.findOne("test1");
-
-        assertThat(test.isPresent()).isEqualTo(true);
-        assertThat(test.get().getPassword()).isEqualTo("123");
-
-        //비밀번호 오류는 위에 비밀번호 확인하는 부분만 다르기에 이 테스트로 비밀번호 오류도 테스팅이 된 것이라 간주.
-    }
-
-    @Test
-    void 로그인_아이디오류(){
-        Member member=new Member();
-        member.setName("test1");
-        member.setUsername("test1");
-        member.setPassword("123");
-        memberService.join(member);
-
-        Optional<Member> test12345 = memberService.findOne("test12345");
-
-        assertThat(test12345.isPresent()).isEqualTo(false);
-    }
+//    @Test
+//    void 중복회원_예외() throws Exception{
+//        Member member=new Member();
+//        member.setName("test1");
+//        member.setUsername("test1");
+//        member.setPassword("123");
+//
+//        Member member2=new Member();
+//        member2.setName("test1");
+//        member2.setUsername("test1");
+//        member2.setPassword("123");
+//
+//        memberService.join(member);
+//
+//        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+//        assertThat(e.getMessage()).isEqualTo("already exist id");
+//    }
+//
+//    @Test
+//    void 로그인(){
+//        Member member=new Member();
+//        member.setName("test1");
+//        member.setUsername("test1");
+//        member.setPassword("123");
+//        memberService.join(member);
+//
+//        Optional<Member> test = memberService.findOne("test1");
+//
+//        assertThat(test.isPresent()).isEqualTo(true);
+//        assertThat(test.get().getPassword()).isEqualTo("123");
+//
+//        //비밀번호 오류는 위에 비밀번호 확인하는 부분만 다르기에 이 테스트로 비밀번호 오류도 테스팅이 된 것이라 간주.
+//    }
+//
+//    @Test
+//    void 로그인_아이디오류(){
+//        Member member=new Member();
+//        member.setName("test1");
+//        member.setUsername("test1");
+//        member.setPassword("123");
+//        memberService.join(member);
+//
+//        Optional<Member> test12345 = memberService.findOne("test12345");
+//
+//        assertThat(test12345.isPresent()).isEqualTo(false);
+//    }
 
 
 }

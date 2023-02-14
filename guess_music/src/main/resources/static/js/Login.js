d8b57f7f3b529d798d17be11ec158f95ea10cdfc
sok5188 <sok5188@gmail.com>
2023-02-15 05:37:02 +0900

    로그인 체크 js삭제

--- guess_music/src/main/resources/static/js/Login.js
+++ guess_music/src/main/resources/static/js/Login.js
@@ -1,17 +1,3 @@
-let url='/auth/checkLogin'
-let notLoginDiv=document.getElementById('notLoginHome');
-let loginDiv=document.getElementById('loginHome');
-fetch(url).then(res=>res.text()).then(data=>{
-    if(data!="False"){
-        notLoginDiv.hidden=true;
-        loginDiv.hidden=false;
-    }else{
-        notLoginDiv.hidden=false;
-        loginDiv.hidden=true;
-    }
-}).catch(err=>{
-    console.log("login check api error");
-})
 
 function logoutHandler(){
     location.href='/logout';

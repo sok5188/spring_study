fa13b3df2dd12cac82c16d64b9edb20e489a8e5f
sok5188 <sok5188@gmail.com>
2023-02-16 10:36:58 +0900

    Login.js to Home.js 파일 이름 수정

---
+++ guess_music/src/main/resources/static/js/Home.js
@@ -0,0 +1,15 @@
+
+function logoutHandler(){
+    location.href='/logout';
+}
+
+function goLogIn(){
+    location.href='/auth/loginForm';
+}
+
+function goSignIn(){
+    location.href='/auth/joinForm';
+}
+function playGame(){
+    location.href='/Game/roomList';
+}

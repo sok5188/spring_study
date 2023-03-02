c2f931e009bd7dcfcb945817f8f3fdeabf3cb5cf
sok5188 <sok5188@gmail.com>
2023-03-03 06:41:32 +0900

    매니저 컨트롤러 테스트 완성

--- guess_music/src/test/java/com/example/guess_music/controller/ManagerControllerTest.java
+++ guess_music/src/test/java/com/example/guess_music/controller/ManagerControllerTest.java
@@ -1,6 +1,10 @@
 package com.example.guess_music.controller;
 
+import com.example.guess_music.domain.game.Answers;
 import com.example.guess_music.domain.game.Game;
+import com.example.guess_music.domain.manage.Music;
+import com.example.guess_music.domain.manage.SaveSongForm;
+import com.example.guess_music.repository.AnswerListMapping;
 import com.example.guess_music.service.ManagerService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -12,6 +16,12 @@ import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -39,14 +49,93 @@ class ManagerControllerTest {
     @DisplayName("API Test")
     class apiTest{
         @Test
+        void 게임삭제() throws Exception {
+            Long gameIndex = makeGame();
+            mockMvc.perform(delete("/manage").param("gameIndex", String.valueOf(gameIndex)))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(content().string("Success"));
+
+        }
+        @Test
         void 게임수정_제목변경() throws Exception{
             Long gameIndex = makeGame();
-            mockMvc.perform(post("/manage/modifyGame").param("gameIndex",gameIndex.toString()).param("title","test"))
+            mockMvc.perform(post("/manage/modifyGame").param("gameIndex",gameIndex.toString()).param("newTitle","test"))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(content().string("Success"));
+        }
+        @Test
+        void 노래삭제() throws Exception{
+            Long gameIndex = makeGame();
+
+            String contentType = "audio/mpeg";
+            String filePath = "testMP3.mp3";
+            MockMultipartFile mockMultipartFile = new MockMultipartFile("mp3","testMP3.mp3",contentType,filePath.getBytes());
+            System.out.println("before store music:"+managerService.getMusicNum(gameIndex));
+            Music music = managerService.storeMusic(mockMultipartFile, gameIndex);
+            System.out.println("after store music:"+managerService.getMusicNum(gameIndex));
+
+            List<String> ansList=new ArrayList<>();
+            ansList.add("test1");
+            ansList.add("test2");
+            Long maxSeq = managerService.storeAnswers(ansList, "testSinger", "testInitial", gameIndex, music);
+
+            System.out.println(managerService.getSongNum(gameIndex));
+            mockMvc.perform(delete("/manage/modifyGame").param("gameIndex", String.valueOf(gameIndex)).param("seq", String.valueOf(maxSeq)))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(content().string("Success"));
+
+            System.out.println("after delete music:"+managerService.getMusicNum(gameIndex));
+            assertThat(managerService.getSongNum(gameIndex)).isEqualTo(0L);
+
+
+        }
+        @Test
+        void 정답추가() throws Exception{
+            Long gameIndex = makeGame();
+
+            List<String> ansList=new ArrayList<>();
+            ansList.add("testAnswer");
+
+            String contentType = "audio/mpeg";
+            String filePath = "testMP3.mp3";
+            MockMultipartFile mockMultipartFile = new MockMultipartFile("mp3","testMP3.mp3",contentType,filePath.getBytes());
+            Music music = managerService.storeMusic(mockMultipartFile, gameIndex);
+
+            Long seq = managerService.storeAnswers(ansList, "testSinger", "testInitial", gameIndex, music);
+            mockMvc.perform(post("/manage/addAnswer").param("seq",seq.toString()).param("answer","newAnswer").param("gameIndex",gameIndex.toString()))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(content().string("Success"));
+            assertThat(managerService.getAnswerNum(gameIndex, seq)).isEqualTo(2L);
+        }
+        @Test
+        void 정답삭제() throws Exception{
+            Long gameIndex = makeGame();
+
+            List<String> ansList=new ArrayList<>();
+            ansList.add("testAnswer");
+
+            String contentType = "audio/mpeg";
+            String filePath = "testMP3.mp3";
+            MockMultipartFile mockMultipartFile = new MockMultipartFile("mp3","testMP3.mp3",contentType,filePath.getBytes());
+            Music music = managerService.storeMusic(mockMultipartFile, gameIndex);
+
+            Long seq = managerService.storeAnswers(ansList, "testSinger", "testInitial", gameIndex, music);
+            Answers ans = managerService.getAnswerForOnlyTest(gameIndex, seq);
+            System.out.println(ans.getId().toString());
+            mockMvc.perform(delete("/manage/updateAnswer").param("ansId", String.valueOf(ans.getId())))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(content().string("Success"));
+            assertThat(managerService.getSongNum(gameIndex)).isEqualTo(0L);
         }
     }
     @Nested
-    @DisplayName("except API Test")
-    class notAPI{
+    @DisplayName("making View Test")
+    class testView{
 
         @Test
         void 게임생성페이지() throws Exception {
@@ -54,17 +143,7 @@ class ManagerControllerTest {
                     .andExpect(status().isOk())
                     .andExpect(view().name("manage/createGame"));
         }
-        @Test
-        void 게임생성() throws Exception {
-            Long beforeIndex = managerService.getValidGameIndex();
-            mockMvc.perform(post("/manage/createGame").param("title","testTitle")).andDo(print())
-                    .andExpect(status().is3xxRedirection())
-                    .andExpect(redirectedUrl("/manage"))
-                    ;
-            Long afterIndex = managerService.getValidGameIndex();
-            System.out.println(beforeIndex+"/"+afterIndex);
-            assertThat(afterIndex).isGreaterThan(beforeIndex);
-        }
+
         @Test
         void 게임수정_상세조회() throws Exception{
             Long gameIndex = makeGame();
@@ -79,15 +158,40 @@ class ManagerControllerTest {
                     .andExpect(status().isOk())
                     .andExpect(view().name("manage/uploadSong"));
         }
-//        @Test
-//        void 노래추가() throws Exception{
-//            Long gameIndex = makeGame();
-//            String fileName = "testMP3";
-//            String contentType = "audio/mpeg";
-//            String filePath = "testMP3.mp3";
-//            MockMultipartFile mockMultipartFile = new MockMultipartFile("testMP3","testMP3.mp3",contentType,filePath.getBytes());
-//            mockMvc.perform(multipart()post("/manage/upload").param("gameIndex",gameIndex.toString()).multi)
-//        }
+        @Test
+        void 노래추가() throws Exception{
+            Long gameIndex = makeGame();
+
+            String contentType = "audio/mpeg";
+            String filePath = "testMP3.mp3";
+            MockMultipartFile mockMultipartFile = new MockMultipartFile("mp3","testMP3.mp3",contentType,filePath.getBytes());
+
+
+            MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+            map.add("answer","testAnswer1");
+            map.add("answer","testAnswer2");
+            map.add("singer","testSinger");
+            map.add("initial","testInitial");
+
+            mockMvc.perform(multipart("/manage/upload").file(mockMultipartFile).params(map).param("gameIndex",gameIndex.toString())).andDo(print())
+                    .andExpect(status().is3xxRedirection())
+                    .andExpect(redirectedUrl("/manage/upload?gameIndex="+gameIndex))
+                    ;
+
+
+        }
+        @Test
+        void 게임생성() throws Exception {
+            Long beforeIndex = managerService.getValidGameIndex();
+            mockMvc.perform(post("/manage/createGame").param("title","testTitle")).andDo(print())
+                    .andExpect(status().is3xxRedirection())
+                    .andExpect(redirectedUrl("/manage"))
+            ;
+            Long afterIndex = managerService.getValidGameIndex();
+            System.out.println(beforeIndex+"/"+afterIndex);
+            assertThat(afterIndex).isGreaterThan(beforeIndex);
+        }
+
 
     }
 

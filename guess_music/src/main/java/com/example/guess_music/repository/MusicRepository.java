4d6391db9c00021bb12e9d1a8a3ceb8c2ba7b2ad
sok5188 <sok5188@gmail.com>
2023-03-03 06:43:39 +0900

    테스트용 쿼리 추가 및 삭제 기능 오류로 인한 수동 삭제 쿼리 추가

--- guess_music/src/main/java/com/example/guess_music/repository/MusicRepository.java
+++ guess_music/src/main/java/com/example/guess_music/repository/MusicRepository.java
@@ -1,7 +1,20 @@
 package com.example.guess_music.repository;
 
+import com.example.guess_music.domain.game.Answers;
 import com.example.guess_music.domain.manage.Music;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface MusicRepository extends JpaRepository<Music, String> {
+    @Query("select a from Music a join fetch a.game g where g.gameIndex=?1")
+    List<Music> findByGameIndex(Long gameIndex);
+    @Query("select count(*) from Music m where m.game.gameIndex=?1")
+    Long findNumberOfMusic(Long gameIndex);
+    @Modifying(clearAutomatically = true)
+    @Query("delete from Music m where m.id=?1")
+    void deleteManual(String id);
 }

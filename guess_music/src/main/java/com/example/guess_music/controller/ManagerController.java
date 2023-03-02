7382045f16b80d5c220ba1a2ac24044d28e454f5
sok5188 <sok5188@gmail.com>
2023-03-03 06:45:04 +0900

    legacy import 삭제

--- guess_music/src/main/java/com/example/guess_music/controller/ManagerController.java
+++ guess_music/src/main/java/com/example/guess_music/controller/ManagerController.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.List;
 @Slf4j
